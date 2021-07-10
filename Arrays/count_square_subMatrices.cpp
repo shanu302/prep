bf31bd2b3d1b86acc0aa0d9fc6129ee4d5bf8a6f
Shanu Kumar Singh <30571196+shanu302@users.noreply.github.com>
2021-07-10 10:09:15 +0530

    Create count_square_subMatrices.cpp

---
+++ Arrays/count_square_subMatrices.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        
+        int res = 0;
+        for(int i=0; i<n;i++){
+          //intilize first row;
+            dp[0][i] = matrix[0][i];
+            res+=dp[0][i];
+        }
+        for(int i=1; i<m;i++){
+          // for first column;
+            dp[i][0] = matrix[i][0];
+            res+=dp[i][0];
+        }
+        for(int i=1; i<m;i++){
+            for(int j=1; j<n;j++){
+                if(matrix[i][j]==1){
+                  // at any point dp[[i][j] contains number of square sub. matrices ending there;
+                    dp[i][j] = min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1])) + 1;
+                }else{
+                    dp[i][j] = 0;
+                }
+                res+=dp[i][j];
+            }
+        }
+        return res;
+    }
+};
