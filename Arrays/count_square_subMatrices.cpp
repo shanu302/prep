501e2cc6052b96ddb887ed9580b126a740d61dfa
Shanu Kumar Singh <30571196+shanu302@users.noreply.github.com>
2021-07-10 10:17:39 +0530

    Update count_square_subMatrices.cpp

--- Arrays/count_square_subMatrices.cpp
+++ Arrays/count_square_subMatrices.cpp
@@ -1,3 +1,5 @@
+// Approach 1; TC: O(M*N) SC: O(M*N)
+
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
@@ -27,6 +29,36 @@ public:
                     dp[i][j] = 0;
                 }
                 res+=dp[i][j];
+            }
+        }
+        return res;
+    }
+};
+
+
+// Approach 2 TC: O(M*N); SC: O(1) (modifying matrix array)
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int res = 0;
+        for(int i=0; i<n;i++){
+           res+= matrix[0][i];
+            
+        }
+        for(int i=1; i<m;i++){
+            res+=matrix[i][0];
+        }
+        for(int i=1; i<m;i++){
+            for(int j=1; j<n;j++){
+                if(matrix[i][j]==1){
+                    matrix[i][j] = min(matrix[i][j-1],min(matrix[i-1][j],matrix[i-1][j-1])) + 1;
+                }else{
+                    matrix[i][j] = 0;
+                }
+                res+=matrix[i][j];
             }
         }
         return res;
